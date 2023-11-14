6ea2413f0d747c46654e80f4e6a01bbf7bdb3712
Guany <1084072690@qq.com>
2023-11-14 09:25:41 +0800

    feat: initial

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { appName } from '~/constants'
+
+useHead({
+  htmlAttrs: {
+    lang: 'zh-CN',
+  },
+  title: appName,
+})
+</script>
+
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { appDescription } from '~/constants'
+</script>
+
+<template>
+  <div>
+    <p>
+      {{ appDescription }}
+    </p>
+  </div>
+</template>
