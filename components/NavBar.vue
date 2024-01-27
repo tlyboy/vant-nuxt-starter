98a0301ec3596c07532bdfc790daf128d5ce0063
Guany <1084072690@qq.com>
2024-01-27 14:31:07 +0800

    chore: update

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,6 +1,7 @@
 <template>
   <nav flex justify-center gap-2 text-xl>
-    <TDarkToggle />
+    <DarkToggle />
+
     <a icon-btn rel="noreferrer" href="https://github.com/tlyboy/vant-nuxt-starter" target="_blank" title="GitHub">
       <div i-carbon-logo-github />
     </a>
